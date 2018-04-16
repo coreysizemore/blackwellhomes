457f98190e50f4bf949f1c5497fa95a8008cb4b3
Corey Sizemore <coreysizemore@gmail.com>
2018-04-16 12:48:33 -0700

    First preview build.

---
+++ page-houses.php
@@ -0,0 +1,17 @@
+<?php
+	
+	get_header();
+	
+	/*
+		@package WordPress
+		@subpackage alderaan
+		Template Name: Page - Houses
+	*/
+	 
+?>
+
+<?php get_template_part( 'headers/header', 'page' ); ?>
+
+<?php get_template_part( 'mains/main', 'houses' ); ?>
+
+<?php get_footer(); ?>
